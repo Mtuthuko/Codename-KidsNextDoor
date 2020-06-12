be28e4247cfe3afcbcfe3c27012ea38bff5fb4e1
sinebongo <37815235+sinebongo@users.noreply.github.com>
2020-06-12 18:23:05 +0200

    added the modified student tml and css page

--- resources/views/dashboard/student.blade.php
+++ resources/views/dashboard/student.blade.php
@@ -2,12 +2,43 @@
 
 @section('content')
 
-
+<link href="/css/student.css" rel="stylesheet">
 <title>Dashboard Students</title>
 
 
 <h1 style="text-align: center">
-    Dashboard Students
+    SUMMATIVE ASSESSMENT SCHEDULE
 </h1>
 
+<div style="overflow-x:auto;">
+<table class="content-table">
+  <thead>
+    <tr>
+      <th>ASSESSMENT CODE</th>
+      <th>COURSE DISCRIPTION</th>
+      <th>ASSESSMENNT TYPE</th>
+      <th>VENUE</th>
+      <th>ASSESSMENT DATE</th>
+      <th>ASSESSMENT TIME</th>
+      <th>DURATION</th>
+      <th>CONCURRENT ASSESSMENT</th>
+      <th>FACULTY</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>COMS</td>
+      <td>WRIITEN</td>
+      <td>EXAM</td>
+      <td>22 DEC 2020</td>
+      <td>9AM</td>
+      <td>3HRS</td>
+      <td>CAM</td>
+      <td>SCIENCE</td>
+  
+    </tr>
+  </tbody>
+</table>
+  </div>
 @endsection
