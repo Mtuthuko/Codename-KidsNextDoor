8bbf1fdb0d7a9d808457c972d5fe6d426a059ec6
Mtuthuko Mngomezulu <parrotphish@localhost.localdomain>
2020-06-14 18:47:08 +0200

    .

--- resources/views/exambookings/dashboard.blade.php
+++ resources/views/exambookings/dashboard.blade.php
@@ -2,6 +2,7 @@
 
   <!-- Testing Code for new dashboard view -->
 @section('content')
+<link href="/css/student.css" rel="stylesheet">
   <!-- Lookup function Form
       "Look up a course in the data base"
 -->
@@ -14,10 +15,13 @@
 
     <form class="lookup-form" action="exambooking" method="get">
       <div class="input-group">
-        <label >Enter CourseCode</label>
+        <label for="search " class="search_label" >Enter CourseCode</label>
         <input type="search" name="search" class="form-control" placeholder="Enter Course Code">
         <span class="input-group-prepend">
-          <button type="submit" class="btn btn-primary">Search</button>
+          <button type="submit" class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="17.49" height="17.49" viewBox="0 0 17.49 17.49">
+  <path id="ic_search_24px" d="M15.5,14h-.79l-.28-.27a6.51,6.51,0,1,0-.7.7l.27.28v.79l5,4.99L20.49,19Zm-6,0A4.5,4.5,0,1,1,14,9.5,4.494,4.494,0,0,1,9.5,14Z" transform="translate(-3 -3)"/>
+</svg>
+</button>
         </span>
       </div>
 
@@ -41,7 +45,7 @@
       <h2 align="center">Courses Data</h1>
       <br />
 
-      <div style="background-color: #0B3865;color:white;border: 1px solid black; padding:5px;border:2px solid white">
+      <div style="overflow-x:auto;">
 
         @if($message = Session::get('success'))
           <div class="alert alert-success">
@@ -49,7 +53,7 @@
           </div>
         @endif
 
-      <table class="table table-bordered">
+      <table class="content-table">
         <tr>
           <th>id</th>
           <th>Cousecode</th>
